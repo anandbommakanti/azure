df568f11e6818c0e7c30eb55a7cc0323d8ae0a89
anandbommakanti <31997156+anandbommakanti@users.noreply.github.com>
2020-05-11 13:14:32 +0530

    Update AddIISandDataDisk.ps1

--- AddIISandDataDisk.ps1
+++ AddIISandDataDisk.ps1
@@ -1,3 +1,3 @@
-﻿#Add-WindowsFeature Web-Server
+Add-WindowsFeature Web-Server
 Get-Disk | Where partitionstyle -eq "raw" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel myDataDisk -Confirm:$false
 
