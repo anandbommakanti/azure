75e5fa075e06f6be830f8558bc50f6d2d1325cce
anandbommakanti <31997156+anandbommakanti@users.noreply.github.com>
2020-05-12 16:16:37 +0530

    Update AddIISandDataDisk.ps1

--- AddIISandDataDisk.ps1
+++ AddIISandDataDisk.ps1
@@ -1,3 +1,9 @@
-Add-WindowsFeature Web-Server
+Add-WindowsFeature Web-Server 
+Install-WindowsFeature -name "Web-Server" -IncludeManagementTool
 Get-Disk | Where partitionstyle -eq "raw" | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel myDataDisk -Confirm:$false
-
+$DriveLetterOfDataDisk = Get-Volume | where FileSystemLabel -eq "myDataDisk" | Select -ExpandProperty DriveLetter
+$DriveLetterOfDataDisk=$DriveLetterOfDataDisk+":"
+$WebDir="$DriveLetterOfDataDisk\VirtualDirectory\TestApp"
+mkdir $WebDir
+echo "<html><body><h1>Hello, Welcome to Azure Virtual Machine</h1></html>" >> $WebDir
+PS F:\> New-WebApplication -name "TestApp" -ApplicationPool "DefaultAppPool" -Force -PhysicalPath $WebDir -Site "Default Web Site"
